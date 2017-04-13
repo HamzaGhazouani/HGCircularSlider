ca9bef0296899a45d2d33023d362ad100a24bb80
Andrzej Michnia <amichnia@gmail.com>
2017-04-13 11:52:24 +0200

    Restored example project. Updated swift syntax.
    Added:
    + example code
    + shadow track
    + number of rounds inspectable
    + example project clock type selection
    Changed:
    * fixed M_PI deprecation warnings

---
+++ Example/HGCircularSlider/ClockViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  HGCircularSlider
+//
+//  Created by Hamza Ghazouani on 10/19/2016.
+//  Copyright (c) 2016 Hamza Ghazouani. All rights reserved.
+//
+
+import UIKit
+import HGCircularSlider
+
+extension Date {
+    
+}
+
+class ClockViewController: UIViewController {
+    
+
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var bedtimeLabel: UILabel!
+    @IBOutlet weak var wakeLabel: UILabel!
+    @IBOutlet weak var rangeCircularSlider: RangeCircularSlider!
+    @IBOutlet weak var clockFormatSegmentedControl: UISegmentedControl!
+    
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup O'clock
+        rangeCircularSlider.startThumbImage = UIImage(named: "Bedtime")
+        rangeCircularSlider.endThumbImage = UIImage(named: "Wake")
+        
+        let dayInSeconds = 24 * 60 * 60
+        rangeCircularSlider.maximumValue = CGFloat(dayInSeconds)
+        
+        rangeCircularSlider.startPointValue = 1 * 60 * 60
+        rangeCircularSlider.endPointValue = 8 * 60 * 60
+
+        updateTexts(rangeCircularSlider)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func updateTexts(_ sender: AnyObject) {
+        let bedtime = TimeInterval(rangeCircularSlider.startPointValue)
+        let bedtimeDate = Date(timeIntervalSinceReferenceDate: bedtime)
+        bedtimeLabel.text = dateFormatter.string(from: bedtimeDate)
+        
+        let wake = TimeInterval(rangeCircularSlider.endPointValue)
+        let wakeDate = Date(timeIntervalSinceReferenceDate: wake)
+        wakeLabel.text = dateFormatter.string(from: wakeDate)
+        
+        let duration = wake - bedtime
+        let durationDate = Date(timeIntervalSinceReferenceDate: duration)
+        dateFormatter.dateFormat = "HH:mm"
+        durationLabel.text = dateFormatter.string(from: durationDate)
+        dateFormatter.dateFormat = "hh:mm a"
+    }
+
+    @IBAction func clockFormatChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: rangeCircularSlider.numberOfRounds = 1
+        case 1: rangeCircularSlider.numberOfRounds = 2
+        default: assert(false)
+        }
+
+        
+    }
+
+}
+
