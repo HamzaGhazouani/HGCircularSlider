1b06a06203626b92f6923edfdc76f242ebfb77e5
isaman kumara <me@isamankumara.com>
2018-03-15 08:51:19 +0530

    add angle text

--- HGCircularSlider/Classes/CircularSlider+Draw.swift
+++ HGCircularSlider/Classes/CircularSlider+Draw.swift
@@ -44,38 +44,38 @@ extension CircularSlider {
      - parameter context: the context
      */
     internal static func drawDisk(withArc arc: Arc, inContext context: CGContext) {
-
+        
         let circle = arc.circle
         let origin = circle.origin
-
+        
         UIGraphicsPushContext(context)
         context.beginPath()
-
+        
         context.setLineWidth(0)
         context.addArc(center: origin, radius: circle.radius, startAngle: arc.startAngle, endAngle: arc.endAngle, clockwise: false)
         context.addLine(to: CGPoint(x: origin.x, y: origin.y))
         context.drawPath(using: .fill)
-
+        
         UIGraphicsPopContext()
     }
-
+    
     // MARK: drawing instance methods
-
+    
     /// Draw the circular slider
     internal func drawCircularSlider(inContext context: CGContext) {
         diskColor.setFill()
         trackColor.setStroke()
-
+        
         let circle = Circle(origin: bounds.center, radius: self.radius)
         let sliderArc = Arc(circle: circle, startAngle: CircularSliderHelper.circleMinValue, endAngle: CircularSliderHelper.circleMaxValue)
         CircularSlider.drawArc(withArc: sliderArc, lineWidth: backtrackLineWidth, inContext: context)
     }
-
+    
     /// draw Filled arc between start an end angles
     internal func drawFilledArc(fromAngle startAngle: CGFloat, toAngle endAngle: CGFloat, inContext context: CGContext) {
         diskFillColor.setFill()
         trackFillColor.setStroke()
-
+        
         let circle = Circle(origin: bounds.center, radius: self.radius)
         let arc = Arc(circle: circle, startAngle: startAngle, endAngle: endAngle)
         
@@ -84,18 +84,18 @@ extension CircularSlider {
         // stroke Arc
         CircularSlider.drawArc(withArc: arc, lineWidth: lineWidth, mode: .stroke, inContext: context)
     }
-
+    
     internal func drawShadowArc(fromAngle startAngle: CGFloat, toAngle endAngle: CGFloat, inContext context: CGContext) {
         trackShadowColor.setStroke()
-
+        
         let origin = CGPoint(x: bounds.center.x + trackShadowOffset.x, y: bounds.center.y + trackShadowOffset.y)
         let circle = Circle(origin: origin, radius: self.radius)
         let arc = Arc(circle: circle, startAngle: startAngle, endAngle: endAngle)
-
+        
         // stroke Arc
         CircularSlider.drawArc(withArc: arc, lineWidth: lineWidth, mode: .stroke, inContext: context)
     }
-
+    
     /**
      Draw the thumb and return the coordinates of its center
      
@@ -110,14 +110,14 @@ extension CircularSlider {
         let thumbOrigin = CircularSliderHelper.endPoint(fromCircle: circle, angle: angle)
         let thumbCircle = Circle(origin: thumbOrigin, radius: thumbRadius)
         let thumbArc = Arc(circle: thumbCircle, startAngle: CircularSliderHelper.circleMinValue, endAngle: CircularSliderHelper.circleMaxValue)
-
+        
         CircularSlider.drawArc(withArc: thumbArc, lineWidth: thumbLineWidth, inContext: context)
         return thumbOrigin
     }
-
+    
     /**
      Draw thumb using image and return the coordinates of its center
-
+     
      - parameter image:   the image of the thumb
      - parameter angle:   the angle of the point in the main circle
      - parameter context: the context
@@ -134,16 +134,16 @@ extension CircularSlider {
         let imageFrame = CGRect(x: thumbOrigin.x - (imageSize.width / 2), y: thumbOrigin.y - (imageSize.height / 2), width: imageSize.width, height: imageSize.height)
         image.draw(in: imageFrame)
         UIGraphicsPopContext()
-
+        
         return thumbOrigin
     }
     
+    
     /**
-     Draw Progress label and return the coordinates of its center
+     Draw thumb using image and return the coordinates of its center
      
+     - parameter image:   the image of the thumb
      - parameter angle:   the angle of the point in the main circle
-     - parameter labelFont:   the font of the label
-     - parameter labelFontColor:   the font color of the label
      - parameter context: the context
      
      - returns: return the origin point of the thumb
@@ -154,33 +154,161 @@ extension CircularSlider {
         context.beginPath()
         let circle = Circle(origin: bounds.center, radius: self.radius)
         let thumbOrigin = CircularSliderHelper.endPoint(fromCircle: circle, angle: angle)
-        let imageSize = CGSize(width:lineWidth , height: lineWidth)
-        let imageFrame = CGRect(x: thumbOrigin.x - (imageSize.width / 2), y: thumbOrigin.y - (imageSize.height / 2), width: imageSize.width, height: imageSize.height)
-        let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
-        textStyle.alignment = NSTextAlignment.center
         
-        let st = "\(String(format: "%.1f", endPointValue))" as NSString
-
-        #if swift(>=4.0)
-            let textFontAttributes = [
-            NSAttributedStringKey.font: labelFont,
-            NSAttributedStringKey.paragraphStyle: textStyle,
-            NSAttributedStringKey.foregroundColor: labelFontColor
-            ]
-            st.draw(in: imageFrame, withAttributes: textFontAttributes)
-        #elseif swift(>=3.0)
-            
-            let textFontAttributes = [
-                NSFontAttributeName: labelFont,
-                NSParagraphStyleAttributeName: textStyle,
-                NSForegroundColorAttributeName: labelFontColor,
-                NSBackgroundColorAttributeName: UIColor.red
-            ]
-            st.draw(in: imageFrame, withAttributes: textFontAttributes)
-        #endif
+        let text = "\(String(format: "%.1f", endPointValue))" as NSString
         
+        let size = self.bounds.size
+        
+        context.translateBy (x: size.width / 2, y: size.height / 2)
+        context.scaleBy (x: 1, y: -1)
+        
+        var textAngle = angle
+        if (angle > 0) {
+            textAngle = angle + CGFloat(Double.pi)
+            if (angle >= CGFloat(Double.pi / 2)) {
+                textAngle = angle * -1
+            } else {
+                textAngle = angle
+                textAngle = textAngle * -1
+            }
+        } else {
+            textAngle = textAngle * -1
+        }
+        
+        centreArcPerpendicular(text: text as String, context: context, radius: radius, angle: textAngle , colour: labelFontColor, font: labelFont , clockwise: true)
         UIGraphicsPopContext()
         
         return thumbOrigin
     }
+    
+    /**
+     This draws the String str around an arc of radius r,
+     
+     - parameter str:   the text need to disply
+     - parameter radius:   the radius need to add label
+     - parameter angle:   the angle of the point in the main circle
+     - parameter c:   the color of the label
+     - parameter font:   the font of the label
+     - parameter clockwise:   the text Are we writing clockwise or not
+     - parameter context: the context
+     
+     */
+    private func centreArcPerpendicular(text str: String, context: CGContext, radius r: CGFloat, angle theta: CGFloat, colour c: UIColor, font: UIFont, clockwise: Bool){
+        
+        // *******************************************************
+        // This draws the String str around an arc of radius r,
+        // with the text centred at polar angle theta
+        // *******************************************************
+        let l = str.count
+        var attributes = [NSAttributedStringKey: Any]()
+        
+        #if swift(>=4.0)
+            attributes = [NSAttributedStringKey.font : font]
+        #elseif swift(>=3.0)
+            attributes = [NSFontAttributeName as NSString: font]
+        #endif
+        
+        let characters: [String] = str.map { String($0) } // An array of single character strings, each character in str
+        var arcs: [CGFloat] = [] // This will be the arcs subtended by each character
+        var totalArc: CGFloat = 0 // ... and the total arc subtended by the string
+        
+        // Calculate the arc subtended by each letter and their total
+        for i in 0 ..< l {
+            var offset = CGSize();
+            #if swift(>=4.0)
+                offset = characters[i].size(withAttributes: attributes)
+            #elseif swift(>=3.0)
+                offset = characters[i].size(attributes: attributes as [String : Any])
+            #endif
+            arcs += [chordToArc(offset.width, radius: r)]
+            totalArc += arcs[i]
+        }
+        
+        
+        // Are we writing clockwise (right way up at 12 o'clock, upside down at 6 o'clock)
+        // or anti-clockwise (right way up at 6 o'clock)?
+        let direction: CGFloat = clockwise ? -1 : 1
+        let slantCorrection = clockwise ? -CGFloat(Double.pi / 2) : CGFloat(Double.pi / 2)
+        
+        // The centre of the first character will then be at
+        //var thetaI = theta - totalArc / 2 + arcs[0] / 2
+        // But we add the last term inside the loop
+        var thetaI = theta - direction * totalArc
+        
+        for i in 0 ..< l {
+            thetaI += direction * arcs[i] / 2
+            // Call centerText with each character in turn.
+            // Remember to add +/-90º to the slantAngle otherwise
+            // the characters will "stack" round the arc rather than "text flow"
+            centre(text: characters[i], context: context, radius: r, angle: thetaI, colour: c, font: font, slantAngle: thetaI + slantCorrection)
+            // The centre of the next character will then be at
+            // thetaI = thetaI + arcs[i] / 2 + arcs[i + 1] / 2
+            // but again we leave the last term to the start of the next loop...
+            thetaI += direction * arcs[i] / 2
+        }
+    }
+    
+    /**
+     Simple geometry
+     
+     */
+    private func chordToArc(_ chord: CGFloat, radius: CGFloat) -> CGFloat {
+        return 2 * asin(chord / (2 * radius))
+    }
+    
+    
+    /**
+     This draws the String str centred at the position
+     
+     - parameter str:   the text need to disply
+     - parameter radius:   the radius need to add label
+     - parameter angle:   the angle of the point in the main circle
+     - parameter c:   the color of the label
+     - parameter font:   the font of the label
+     - parameter slantAngle:   the text angle
+     - parameter context: the context
+     
+     */
+    private func centre(text str: String, context: CGContext, radius r:CGFloat, angle theta: CGFloat, colour c: UIColor, font: UIFont, slantAngle: CGFloat) {
+        // *******************************************************
+        // This draws the String str centred at the position
+        // specified by the polar coordinates (r, theta)
+        // i.e. the x= r * cos(theta) y= r * sin(theta)
+        // and rotated by the angle slantAngle
+        // *******************************************************
+        
+        // Set the text attributes
+        var attributes = [NSAttributedStringKey: Any]()
+        
+        #if swift(>=4.0)
+            attributes = [NSAttributedStringKey.font : font,
+            NSAttributedStringKey.foregroundColor: c]
+        #elseif swift(>=3.0)
+            attributes = [NSFontAttributeName as NSString: font,
+                          NSForegroundColorAttributeName as NSString: c]
+        #endif
+        
+        // Save the context
+        context.saveGState()
+        // Undo the inversion of the Y-axis (or the text goes backwards!)
+        context.scaleBy(x: 1, y: -1)
+        // Move the origin to the centre of the text (negating the y-axis manually)
+        context.translateBy(x: r * cos(theta), y: -(r * sin(theta)))
+        // Rotate the coordinate system
+        context.rotate(by: -slantAngle)
+        // Calculate the width of the text
+        var offset = CGSize();
+        #if swift(>=4.0)
+            offset = str.size(withAttributes: attributes)
+        #elseif swift(>=3.0)
+            offset = str.size(attributes: attributes as [String : Any])
+        #endif
+        // Move the origin by half the size of the text
+        context.translateBy (x: -offset.width / 2, y: -offset.height / 2) // Move the origin to the centre of the text (negating the y-axis manually)
+        // Draw the text
+        str.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes as [String : Any])
+        // Restore the context
+        context.restoreGState()
+    }
 }
+
