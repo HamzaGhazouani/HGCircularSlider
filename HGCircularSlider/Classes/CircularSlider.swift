1cf18763ebdb2524e445f22fdf0dc19dd52f256a
Christos Koninis <christos.koninis@gmail.com>
2019-07-24 02:54:20 +0300

    Added thumb offset option (#52)
    Add the option to draw the thumb inside or outside the circle

--- HGCircularSlider/Classes/CircularSlider.swift
+++ HGCircularSlider/Classes/CircularSlider.swift
@@ -171,7 +171,21 @@ open class CircularSlider: UIControl {
             }
         }
     }
-    
+
+    /**
+    * The offset of the thumb centre from the circle.
+    *
+    * You can use this to move the thumb inside or outside the circle of the slider
+    * If the value is grather than 0 the thumb will be displayed outside the cirlce
+    * And if the value is negative, the thumb will be displayed inside the circle 
+    */
+    @IBInspectable
+    open var thumbOffset: CGFloat = 0.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
     /**
      * The value of the endThumb (changed when the user change the position of the end thumb)
      *
@@ -203,8 +217,12 @@ open class CircularSlider: UIControl {
         get {
             // the minimum between the height/2 and the width/2
             var radius =  min(bounds.center.x, bounds.center.y)
+            
+            // if we use an image for the thumb, the radius of the image will be used
+            let maxThumbRadius = max(thumbRadius, (self.endThumbImage?.size.width  ?? 0) / 2)
+
             // all elements should be inside the view rect, for that we should subtract the highest value between the radius of thumb and the line width
-            radius -= max(lineWidth, (thumbRadius + thumbLineWidth))
+            radius -= max(lineWidth, (maxThumbRadius + thumbLineWidth + thumbOffset))
             return radius
         }
     }
